b5b52642642e34f3af295124f7e72229b8e3feec
Barry Zhang <45883553+bluebarryz@users.noreply.github.com>
2025-02-15 15:41:31 -0500

    add nav2 files

--- watod-config.sh
+++ watod-config.sh
@@ -16,6 +16,8 @@
 ##   - samples          :   starts up sample nodes for reference
 
 # ACTIVE_MODULES=""
+ACTIVE_MODULES="robot gazebo vis_tools localization"
+
 
 ################################# MODE OF OPERATION #################################
 ## Possible modes of operation when running watod.
@@ -38,4 +40,4 @@
 
 ## Platform in which to build the docker images with. 
 ## Either arm64 (apple silicon, raspberry pi) or amd64 (most computers)
-# PLATFORM="amd64"
+PLATFORM="arm64"
